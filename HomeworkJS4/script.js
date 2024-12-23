66b1b40b2cffbbac1dd9aac972ba972c81217687
s1ckys <sinisa.bogdanovski.1998@gmail.com>
2024-12-23 20:06:14 +0100

    yes

---
+++ HomeworkJS4/script.js
@@ -0,0 +1,14 @@
+let mainHeader = document.querySelector("#first > h1");
+mainHeader.textContent = "My first DOM manipulation";
+let firstParagraph = document.querySelector(".paragraph")
+firstParagraph.textContent = "This is really easy";
+let secondParagraph = document.querySelector(".second");
+secondParagraph.textContent = "This exercise is great!";
+let secondHeader = document.querySelector(".anotherDiv + div h1");
+secondHeader.textContent = "You should try it yourself";
+let thirdHeader = document.querySelector(".anotherDiv + div h3");
+thirdHeader.textContent = "I am getting used to this"
+
+
+
+
